b6505f83e19cd48da28e05990aaffcb626cd2ca0
Corbin Simpson <cds@corbinsimpson.com>
2016-02-24 20:30:29 -0800

    Switch to mt-bake.
    The generated executable chews CPU for some reason.

--- default.nix
+++
@@ -1,9 +0,0 @@
-{ system ? builtins.currentSystem, typhonPackage ? ~/typhon/default.nix, ... }:
-let
-  nixpkgs = import <nixpkgs> { inherit system; };
-  typhon = nixpkgs.callPackage typhonPackage {};
-  jobs = with nixpkgs; {
-    playpen = callPackage ./playpen.nix { inherit typhon; };
-  };
-in
-  jobs
